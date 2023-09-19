b889621a27235d1dd5817ac8b619c6ad90ca4f76
Patryk Sąsiadek <100174940+kimi1723@users.noreply.github.com>
2023-09-19 14:43:34 +0200

    exporting/importing slices

---
+++ Redux/react-redux-project/src/store/counter.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialCounterState = { counter: 0, showCounter: true };
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
