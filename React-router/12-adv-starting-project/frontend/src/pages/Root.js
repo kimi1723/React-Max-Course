98c3b5a87ba3129398d238c7ba7c8cedd9e56901
Patryk Sąsiadek <patrykskontakt@gmail.com>
2023-09-30 21:18:36 +0200

    useNavigation

--- React-router/12-adv-starting-project/frontend/src/pages/Root.js
+++ React-router/12-adv-starting-project/frontend/src/pages/Root.js
@@ -1,12 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 export const Root = () => {
+	const navigation = useNavigation();
+
 	return (
 		<>
 			<MainNavigation />
-			<Outlet />
+			<main>
+				<Outlet />
+				{navigation.state === 'loading' && <p>Loading...</p>}
+			</main>
 		</>
 	);
 };
